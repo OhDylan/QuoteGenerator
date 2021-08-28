55a42bd2a48c57536c04bfb2351001fadf2f1430
Oh Dylan <ddragon1128@hotmail.com>
2021-08-28 23:05:00 +0800

    Completed quote generator function and twitting function

--- script.js
+++ script.js
@@ -1,12 +1,59 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+let apiQuotes = []
+
+// Show new quote
+function newQuote(){
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    // Check if author is blank, replace with 'Unknown'
+    if(!quote.author)
+    {
+        authorText.textContent = 'Unknown'
+    }
+    else
+    {
+        authorText.textContent = quote.author
+    }
+
+    // Check quote length to determine styling
+    if(quote.text.length > 120)
+    {
+        quoteText.classList.add('long-quote')
+    }
+    else
+    {
+        quoteText.classList.remove('long-quote')
+    }
+    quoteText.textContent = quote.text
+}
+
 // Get Quotes from API
 async function getQuotes(){
-    const apiUrl = 'https://type.fit/api/quotes';
+    const apiUrl = 'https://type.fit/api/quotes'
     try
     {
-        const response = await fetch(apiUrl);
+        const response = await fetch(apiUrl)
+        apiQuotes = await response.json()
+        newQuote()
     }
     catch (error)
     {
         console.error(error)
     }
 }
+
+// To tweet quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+    window.open(twitterUrl, '_blank')
+}
+
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+
+// On Load
+getQuotes()
