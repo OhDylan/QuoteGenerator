1831221adfd8ae80f1d8d0fa3406327963430609
Oh Dylan <ddragon1128@hotmail.com>
2021-08-29 13:56:27 +0800

    Added loading spinner

--- script.js
+++ script.js
@@ -4,10 +4,25 @@ const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
 
+const loader = document.getElementById('loader')
+
 let apiQuotes = []
+
+// Show loading
+function loading(){
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+
+// Hide loading
+function complete(){
+    loader.hidden = true
+    quoteContainer.hidden = false
+}
 
 // Show new quote
 function newQuote(){
+    loading()
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
     // Check if author is blank, replace with 'Unknown'
     if(!quote.author)
@@ -28,11 +43,14 @@ function newQuote(){
     {
         quoteText.classList.remove('long-quote')
     }
+    // Set quote and hide loader
     quoteText.textContent = quote.text
+    complete()
 }
 
 // Get Quotes from API
 async function getQuotes(){
+    loading()
     const apiUrl = 'https://type.fit/api/quotes'
     try
     {
